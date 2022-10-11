a59b9cfc905e2bc936e489c7f351cd576479f447
mirzanahid <mirzasaikat3@gmail.com>
2022-10-11 14:07:44 +0600

    topic card added

--- src/components/SingleTopic/SingleTopic.js
+++ src/components/SingleTopic/SingleTopic.js
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 
 import './SingleTopic.css'
 
-const SingleTopic = ({topic}) => {
-    const {id,logo, name, total} = topic;
+const SingleTopic = ({ topic }) => {
+    const { id, logo, name, total } = topic;
     return (
-     
- 
+
+
         <div className='single-topic'>
+            <div className='single-topic-top'>
             <div className="logo-container">
-                <img src={logo} alt="" />
+                    <img src={logo} alt="" />
+                </div>
             </div>
-         
-            <h3 className='topic-heading'>{name}</h3>
-            <p className='topic-quiz'>Quiz:{total}</p>
-           <Link to={`quiz/${id}`}> <button className='topic-btn'>Start Quiz</button></Link>
+            <div className='single-topic-info'>
+                
+                <h3 className='topic-heading'>{name}</h3>
+                <p className='topic-quiz'>Total Quiz: {total}</p>
+                <Link to={`quiz/${id}`}> <button className='topic-btn'>Start Quiz</button></Link>
+            </div>
         </div>
-     
+
     );
 };
 
